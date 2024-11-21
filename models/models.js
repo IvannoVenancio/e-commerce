577d96cb4dbadeb6a8e9490538efca6b6093324d
ARi Black <ariblack298@gmail.com>
2024-11-21 21:41:05 +0100

    Adicionando e atualizando arquivos

---
+++ models/models.js
@@ -0,0 +1,12 @@
+
+let modelsJson = [
+    {id:1, name:'Iphone 16 pro', img:'img/iPhone_16_Pro.png', price:[100.00, 110.00, 150.00, 120.00], sizes:['1/72', '1/48', '1/32', '1/20'], description:'Iphone 16 pro de 64GB,Proces­sador A18 Pro com GPU 6‑core,Bateria Até 33 horas de vídeo '},
+    {id:2, name:'Iphone 14', img:'img/iPhone 14 - 2.png', price:[100.00, 150.00, 200.00], sizes:['1/12', '1/34', '1/6', '1/9'], description:'Iphone 14 de 128GB,Proces­sador A15 Bionic,Bateria até 20 horas de vídeo'},
+    {id:3, name:'Iphone Xr', img:'img/iphone xr preto.png', price:[100.00, 150.00, 200.00], sizes:['1/72', '1/48', '1/32', '1/4'], description:'Iphone xr de 32GB, Proces­sador A12 Bionic, Bateria até 15 horas de vídeo'},
+    {id:4, name:'Iphone 11', img:'img/2-4.png', price:[100.00, 150.00, 200.00], sizes:['1/72', '1/48', '1/32','1/6'], description:'Iphone 11 normal de 128GB,Proces­sador A13 Bionic,Bateria até 16 horas de vídeo'},
+    {id:5, name:'Iphone Xr', img:'img/iphone vermelho.png', price:[100.00, 150.00, 200.00], sizes:['1/72', '1/48', '1/32', '1/3'], description:'Iphone xr de 256GB,Proces­sador A12 Bionic,Bateria até 15 horas de vídeo'},
+    {id:6, name:'MacBook pro 14 2023 M3', img:'img/MacBook pro 14.png', price:[100.00, 150.00, 200.00], sizes:['1/72', '1/48', '1/32'], description:'8-core CPU with 4 performance cores, 16-core Neural Engine, 100GB/s memory'},
+    {id:7, name:'Tablet 10.1', img:'img/tablet 1.png', price:[100.00, 150.00, 200.00], sizes:['1/72', '1/48', '1/32'], description:'32 GB - 2 GB RAM - Wi-Fi, pesando apenas 565 gramas e tem uma espessura de 8.6 mm'},
+    {id:8, name:'Computador Lenovo Portatil', img:'img/Lenovo-ThinkPad-T15g-laptop-RTX (1).png', price:[100.00, 150.00, 200.00], sizes:['1/72', '1/48', '1/32'], description:'8GB, DDR4, V15-IRU G4 15.6 i3-1315U FHD 8GB 256GB W11P, INTEL CORE I3'},
+    {id:9, name:'Computador Gamer', img:'img/high-performance-gaming-setup-yf.png', price:[100.00, 110.00, 120.00], sizes:['1/72', '1/48', '1/32'], description:'Um computador para jogos com iluminação LED, microfone e fone de ouvido.'}
+];
