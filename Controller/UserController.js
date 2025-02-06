ffe70e37ecb0f5618f6efa23dec8a548a0370f2f
Deogamex <deogamex@gmail.com>
2025-02-06 14:14:19 +0000

    Resolução de bugs e conflitos na rota do login, alteração de estilo da pag

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,11 +1,11 @@
 const { createUser, findAllUsers } = require("../services/userService")
 
-exports.view = async(req, res) =>{
+exports.view = async (req, res) => {
     try {
-        const users = await findAllUsers()
-        res.render('home', {users})        
+        res.render("login", { layout: "clogin" }); // Defina o layout corretamente aqui
     } catch (error) {
-        
+        console.log("Erro ao renderizar a página de login:", error);
+        res.status(500).send("Erro interno do servidor");
     }
 }
 exports.create = async(req, res) =>{
