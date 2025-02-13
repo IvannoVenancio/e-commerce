43374a11f0101c5cad94186f890941ff16fc8d87
[António Nunes] <[Antonio-dias-nunes@hotmail.com]>
2025-02-13 12:23:27 +0100

    Actualização124

--- Controller/GlobalController.js
+++ Controller/GlobalController.js
@@ -7,7 +7,7 @@ exports.home = async(req, res) => {
 }
 exports.cadastro = async(req, res) => {    
     try {
-        res.render("cadastro")  
+        res.render("cadastro",{layout:"ccadastro"})  
     }catch(err){
         console.log(err)
     }      
@@ -21,14 +21,22 @@ exports.Listproduto = async(req, res) => {
 }
 exports.carrinho = async(req, res) => {    
     try {
-        res.render("carrinho",{layout:"cdmanin.handlebars"})  
+        res.render("carrinho",{layout:"cdmanin"})  
     }catch(err){
         console.log(err)
     }      
 }
+exports.feedback = async (req, res) => {
+    try {
+        res.render("feedback", { layout: "feedmain.handlebars" });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 exports.login = async(req, res) => {    
     try {
-        res.render("login",{layout:"cdmanin.handlebars"})  
+        res.render("login",{layout:"clogin"})  
     }catch(err){
         console.log(err)
     }      
@@ -40,3 +48,19 @@ exports.Pedidos = async(req, res) => {
         console.log(err)
     }      
 }
+exports.Pedidos = async(req, res) => {    
+    try {
+        res.render("Pedidos")  
+    }catch(err){
+        console.log(err)
+    } 
+     
+}
+exports.pagamento = async(req, res) => {    
+    try {
+        res.render("pagamento" ,{layout:"ccadastro"})  
+    }catch(err){
+        console.log(err)
+    } 
+}
+
